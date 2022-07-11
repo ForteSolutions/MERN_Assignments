b47a34629aa102a45c0e115a07ebe72076f71489
ForteSolutions <kforte1914@gmail.com>
2022-07-11 19:48:06 -0400

    First Commit

---
+++ Predict_Destructuring.js
@@ -0,0 +1,58 @@
+// Problem 1
+const cars = ['Tesla', 'Mercedes', 'Honda']
+const [ randomCar ] = cars
+const [ ,otherRandomCar ] = cars
+//Predict the output
+console.log(randomCar)
+console.log(otherRandomCar)
+//Prediction: Should return Tesla and Mercades because they are the first two in the array
+
+// Problem 2
+const employee = {
+    name: 'Elon',
+    age: 47,
+    company: 'Tesla'
+}
+const { name: otherName } = employee;
+//Predict the output
+console.log(employee.name);
+console.log(otherName);
+//Prediciton: Should return as an error because name is under employee
+
+//Problem 3
+const person = {
+    name: 'Phil Smith',
+    age: 47,
+    height: '6 feet'
+}
+const password = '12345';
+const { password: hashedPassword } = person;  
+//Predict the output
+console.log(password);
+console.log(hashedPassword);
+//Predicton: The hashed Password should return undefined because nothing in the person variable is defined, however password will return 12345
+
+//Problem 4
+const numbers = [8, 2, 3, 5, 6, 1, 67, 12, 2];
+const [,first] = numbers;
+const [,,,second] = numbers;
+const [,,,,,,,,third] = numbers;
+//Predict the output
+console.log(first == second);
+console.log(first == third);
+//Prediction: One will come back true and one will come back as false
+
+//Problem 5
+const lastTest = {
+    key: 'value',
+    secondKey: [1, 5, 1, 8, 3, 3]
+}
+const { key } = lastTest;
+const { secondKey } = lastTest;
+const [ ,willThisWork] = secondKey;
+//Predict the output
+console.log(key);
+console.log(secondKey);
+console.log(secondKey[0]);
+console.log(willThisWork);
+//Prediction: key should come back with 'value', secondkey should back with the whole array, secondkey[0] will come back with the first position, which is 1, and willThisWork returns the second one in the array, which is 5
