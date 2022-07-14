071e2cc460d86ab6e1a521f0885177cdebead66b
ForteSolutions <kforte1914@gmail.com>
2022-07-14 19:49:40 -0400

    Putting It Together

---
+++ putting-it-together/src/components/PersonCard.js
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+
+const PersonCard = (props) => {
+    const [age, setAge] = useState(props.age)
+
+    const birthday = () => {
+        setAge(age + 1);
+    }
+
+    return (
+        <div>
+            <h2>{props.lastName}, {props.firstName}</h2>
+            <p>Age: {age}</p>
+            <p>Hair Color: {props.hairColor}</p>
+            <button onClick={(event) => setAge(parseInt(age) + 1)}>Birthday Button for {props.firstName} {props.lastName}</button>
+        </div>
+    )
+}
+
+export default PersonCard
