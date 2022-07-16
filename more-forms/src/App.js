d9bce6e2cc633dc778b0161708d315f6b12da518
ForteSolutions <kforte1914@gmail.com>
2022-07-15 21:43:19 -0400

    More Forms

---
+++ more-forms/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import React, { useState } from 'react';
+import Form from './components/Form';
+
+function App() {
+  const [state, setState] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    confirmPassword: ""
+  });
+  return (
+    <div className="App">
+      <Form inputs={state} setInputs={setState}/>
+    </div>
+  );
+}
+
+export default App;
