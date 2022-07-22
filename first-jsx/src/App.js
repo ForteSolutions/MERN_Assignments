816fa22237d4e38072cda37153401d36107d5277
ForteSolutions <kforte1914@gmail.com>
2022-07-22 13:53:38 -0400

    First JSX

--- first-jsx/src/App.js
+++ first-jsx/src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <h1>Hello Dojo!</h1>
-      <h2>Things I need to do:</h2>
+      <h3>Things I need to do:</h3>
       <ul>
           <li>Learn React</li>
           <li>Climb Mt. Everest</li>
