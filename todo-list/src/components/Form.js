18b1a3939bc59f17ef6df4abf5bcccd4157a416a
ForteSolutions <kforte1914@gmail.com>
2022-07-25 08:33:52 -0400

    Todo List

---
+++ todo-list/src/components/Form.js
@@ -0,0 +1,31 @@
+import React, {useState} from 'react';
+
+const Form = (props)=>{
+
+    const [todoText, setTodoText] = useState("");
+
+    const {todoList, setTodoList} = props;
+
+const submitHandler = (e) =>{
+    e.preventDefault();
+
+    setTodoList([...todoList, {
+        content: todoText,
+        markedDelete: false,
+        id: Math.floor(Math.random() * 100000000)
+    }])
+
+    setTodoList("");
+}
+
+    return(
+        <div>
+            <form onSubmit={submitHandler}>
+                <input value={todoText} onChange={(e)=>setTodoText(e.target.value)} type="text"/>
+                <button>Add</button>
+            </form>
+        </div>
+    )
+}
+
+export default Form;
