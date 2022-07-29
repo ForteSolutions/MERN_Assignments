e836fd6b80d9bbd7769ff3dbf3a0044d9b0a7dbf
ForteSolutions <kforte1914@gmail.com>
2022-07-29 07:54:34 -0400

    Pokemon API

---
+++ pokemon-api/src/App.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+
+    const [pokemons, setPokemons] = useState({});
+    const [sendRequest, setSendRequest] = useState(false);
+
+    useEffect(() => {
+        if(sendRequest){
+            fetch('https://pokeapi.co/api/v2/pokemon/?limit=807')
+                .then(response => response.json())
+                .then(response => {
+                    setPokemons(response.results)
+                })
+                .catch()
+            setSendRequest(false);
+        }
+    }, [sendRequest]);
+
+    const onClick = (e) => {
+        setSendRequest(true)
+    }
+
+    return (
+        <div className="App bg-light p-3">
+            <div className="w-25 mt-2 mx-auto">
+                <button className="btn btn-secondary mb-3" onClick={onClick}> Fetch Pokemon</button>
+                <p className="mb-1">List of Pokemons: </p>
+                <ol>
+                    {pokemons.length > 0 && pokemons.map((pokemon, index)=>
+                        <li key={index}>{pokemon.name}</li>
+                    )}
+                </ol>
+            </div>
+        </div>
+    );
+}
+
+export default App;
