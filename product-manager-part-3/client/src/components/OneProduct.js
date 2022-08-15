32059d3c800931ab942fbbc5fa9a50e2bee52862
ForteSolutions <kforte1914@gmail.com>
2022-08-14 23:57:24 -0400

    Product Manager (Part 3)

---
+++ product-manager-part-3/client/src/components/OneProduct.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useParams, Link } from 'react-router-dom';
+
+const OneProduct = (props)=>{
+
+    const {id} = useParams();
+
+    const [oneProduct, setOneProduct] = useState({});
+
+    useEffect(()=>{
+        axios.get(`http://localhost:8000/api/products/${id}`)
+            .then((res)=>{
+                console.log(res);
+                console.log(res.data);
+                setOneProduct(res.data);
+            })
+            .catch((err)=>console.log(err))
+    },[id])
+
+    return(
+        <div className="oneProduct-component">
+            <h2>{oneProduct.title}</h2>
+            <p>Price: ${oneProduct.price}</p>
+            <p>Description: {oneProduct.description}</p>
+            <Link to={"/"}>Go Home</Link>
+        </div>
+        )
+}
+
+export default OneProduct;
