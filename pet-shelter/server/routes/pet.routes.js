8cb9cba570aea68212cc2a8e72d445795928ae68
ForteSolutions <kforte1914@gmail.com>
2022-09-03 16:13:47 -0400

    Delete

--- pet-shelter/server/routes/pet.routes.js
+++
@@ -1,9 +0,0 @@
-const petController = require("../controllers/pet.controller");
-
-module.exports = (app) => {
-    app.post("/api/pet", petController.createNewPet);
-    app.get("/api/pet", petController.getAllPets);
-    app.get("/api/pet/:id", petController.getOnePet);
-    app.put("/api/pet/:id", petController.updatePet);
-    app.delete("/api/pet/:id", petController.deletePet);
-    };
