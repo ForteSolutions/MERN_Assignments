2d79fca8f1f5c06f03b50e63ce1512b6db5874d9
ForteSolutions <kforte1914@gmail.com>
2022-09-05 02:20:12 -0400

    Strings To Do 1

---
+++ strings-to-do-1.js
@@ -0,0 +1,28 @@
+// Strings To Do 1
+// Write functions using the JavaScript language for all challenges. Use only a single JavaScript file for this assignment. Do not use built-in methods (unless otherwise instructed). (.length is a property, and is allowed.)
+
+// Remove Blanks
+// Create a function that, given a string, returns all of that string’s contents, but without blanks. 
+
+// Examples:
+
+// removeBlanks(" Pl ayTha tF u nkyM usi c ") => "PlayThatFunkyMusic"
+// removeBlanks("I can not BELIEVE it's not BUTTER") => "IcannotBELIEVEit'snotBUTTER"
+
+function removeBlanks(str) {
+   // make a new string initialized to a blank string
+   let newStr = ''
+
+   // loop through the given string
+   for(let char in str) {
+    if(str[char] != ' ') {
+        newStr += str[char]
+    }
+}
+
+    //return new string
+    return newStr
+}
+
+console.log(removeBlanks(" Pl ayTha tF u nkyM usi c "));
+console.log(removeBlanks("I can not BELIEVE it's not BUTTER"));
